1d3128cd57e6730e65c2121836249ccd8098d887
James Easter <james.m.easter@gmail.com>
2020-06-28 14:54:59 -0500

    (feat) add animation (color & timing) to bubble sort

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,9 @@ export default {
   data() {
     return {
       numbers: [],
+      primary: "dodgerblue",
+      compare: "#66FCF1",
+      sorted: "#A768C4",
     };
   },
   created() {
@@ -33,21 +36,23 @@ export default {
   methods: {
     populateArray: function() {
       this.numbers = [];
-      // randomize the length of the array
+      // randomize the length of the array FOR TESTING
       // let length = Math.round(Math.random() * 5) + 20;
 
       for (let i = 0; i < 25; i++) {
-        let randomNumber = Math.round(Math.random() * 90) + 10;
-        this.numbers.push(randomNumber);
+        let value = Math.round(Math.random() * 90) + 10;
+        let color = this.primary;
+        this.numbers.push({ value, color });
       }
     },
     test: function() {
+      // REMOVE ANY AWAIT KEYWORDS FOR TESTING
       for (let i = 0; i < 100; i++) {
         this.populateArray();
         let testArray = [...this.numbers];
 
         // test out different functions here
-        this.bubbleSort();
+        // this.bubbleSort();
 
         testArray.sort((a, b) => a - b);
         let result =
@@ -56,34 +61,59 @@ export default {
         console.log(result);
       }
     },
-    bubbleSort: function() {
-      let swap = true;
+    bubbleSort: async function() {
       let counter = 0;
 
-      while (swap) {
-        swap = false;
+      while (counter < this.numbers.length) {
         for (let i = 0; i < this.numbers.length - 1 - counter; i++) {
-          if (this.numbers[i] > this.numbers[i + 1]) {
-            swap = true;
-            let placeholder = this.numbers[i + 1];
-            this.$set(this.numbers, i + 1, this.numbers[i]);
-            this.$set(this.numbers, i, placeholder);
+          // change color of two indeces that are being compared
+          let { value: a } = this.numbers[i];
+          let { value: b } = this.numbers[i + 1];
+          this.$set(this.numbers, i, { value: a, color: this.compare });
+          this.$set(this.numbers, i + 1, { value: b, color: this.compare });
+
+          // pauses the event loop to better visualize the algo
+          await new Promise((resolve) => setTimeout(resolve, 50));
+
+          // if the first index is greater than the second
+          if (this.numbers[i].value > this.numbers[i + 1].value) {
+            // swap indeces
+            let { value, color } = this.numbers[i];
+            let { value: tempValue } = this.numbers[i + 1];
+            this.$set(this.numbers, i + 1, { value, color });
+            this.$set(this.numbers, i, { value: tempValue, color });
+
+            // pauses the event loop to better visualize the algo
+            await new Promise((resolve) => setTimeout(resolve, 20));
           }
+
+          // change colors back to primary and set the final index color to sorted
+          let { value: newA } = this.numbers[i];
+          let { value: newB } = this.numbers[i + 1];
+          this.$set(this.numbers, i, { value: newA, color: this.primary });
+          this.$set(this.numbers, i + 1, { value: newB, color: this.sorted });
         }
+        // increment counter
         counter += 1;
       }
+      // change the color to sorted on the final iteration (first index)
+      let { value } = this.numbers[0];
+      this.$set(this.numbers, 0, { value, color: this.sorted });
     },
   },
 };
 </script>
 
 <style>
+* {
+  background-color: #17252a;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #feffff;
   margin-top: 60px;
 }
 </style>
