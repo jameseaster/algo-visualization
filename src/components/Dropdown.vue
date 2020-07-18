e2605e0759e5c6a66feddcd4dd5cf11c53d3415e
James Easter <james.m.easter@gmail.com>
2020-07-18 14:05:28 -0500

    (feat) add more info button and style w/ bootstrap

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,14 +1,8 @@
 <template>
-  <b-container>
-    <b-row>
-      <b-col>
-        <b-form-select
-          v-model="selected"
-          :options="options"
-          size="sm"
-          class="mt-3 w-50"
-          sm="6"
-        >
+  <b-container fluid="sm">
+    <b-row align-h="center" class="mt-2" no-gutters>
+      <b-col cols="6" md="4" lg="3" align-self="center" fluid>
+        <b-form-select v-model="selected" :options="options">
           <template v-slot:first>
             <b-form-select-option :value="null" disabled>
               Select An Algorithm
@@ -16,20 +10,27 @@
           </template>
         </b-form-select>
       </b-col>
-    </b-row>
-    <b-row align-v="center">
-      <b-col></b-col>
-      <b-col
-        ><b-button class="m-1" @click="$emit('new-array')">
-          New Array
-        </b-button></b-col
-      >
-      <b-col
+      <b-col cols="auto" align-self="center" class="mx-2"
         ><b-button :value="selected" @click="$emit(selected)"
           >Go!</b-button
         ></b-col
       >
-      <b-col></b-col>
+    </b-row>
+    <b-row align-h="center" class="mt-2" no-gutters>
+      <b-col cols="auto" align-self="center" class="mx-2"
+        ><b-button @click="$emit('new-array')">New Array</b-button></b-col
+      >
+      <b-col cols="auto" align-self="center">
+        <b-button v-b-toggle.collapse-1>More Info</b-button></b-col
+      >
+    </b-row>
+    <b-row align-h="center">
+      <b-collapse id="collapse-1" class="mt-2">
+        <b-card>
+          <p v-if="selected === null">Pick a sorting algorithm!</p>
+          <p v-else class="card-text">Info on {{ selected }} sort to come!</p>
+        </b-card>
+      </b-collapse>
     </b-row>
   </b-container>
 </template>
