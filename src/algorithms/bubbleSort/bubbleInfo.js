d5ea951ff470bec8d7c2829269e8c060d1c32d09
James Easter <james.m.easter@gmail.com>
2020-08-02 14:11:40 -0500

    (cleanup) make minor edits to more info on each algorithm

--- src/algorithms/bubbleSort/bubbleInfo.js
+++ src/algorithms/bubbleSort/bubbleInfo.js
@@ -1,13 +1,13 @@
 function bubbleInfo() {
   return `Bubble sort gets its name because smaller elements
-  bubble toward the top of the list. This algorithm compares
+  bubble toward the top of their list. This algorithm compares
   two adjacent values. If the first one is larger than the
   second, then it will swap those two values. After repeating
   this process all of the way through the data set, the largest
   value will have been continually swapped to the very end,
-  arriving in it's final sorted position. Repeating this process
-  the same number of times as there are values to be sorted
-  will result in a sorted array.`;
+  arriving in its final sorted position. This complete iteration
+  is repeated the same number of times as there are values in the
+  unsorted list.`;
 }
 
 module.exports = bubbleInfo;
--- src/algorithms/heapSort/heapInfo.js
+++ src/algorithms/heapSort/heapInfo.js
@@ -1,12 +1,13 @@
 function heapInfo() {
-  return `Heap sort utilizes the heap data structure. As step
-  one, heap sort creates a max heap out of the given values.
-  As a rule of thumb, a max heap always pushes the largest value
-  to the top. Heap sort will take the value off of the top of the
-  max heap and place it on the far right. The max heap will then
-  reorganize itself by pushing the next biggest value to the top
-  of its structure. This process is repeated until the max heap
-  runs hands off all of its values to the now sorted collection.`;
+  return `Heap sort utilizes the heap data structure. In fact, step
+  one to this algorithm is creating a max heap out of the unsorted
+  values. As a rule of thumb, a max heap always pushes the largest
+  value to the top of its structure. Heap sort will take the value
+  off of the top of the max heap and place it on the far right of
+  the list. Then, the max heap will then reorganize itself by pushing
+  the next biggest value to the top of its structure. This process
+  is repeated until the max heap has handed off all of its values
+  to the now sorted collection.`;
 }
 
 module.exports = heapInfo;
