21665bd11e8d7b80caa438dd4ccce9668d332f6d
James Easter <james.m.easter@gmail.com>
2020-08-06 08:53:27 -0500

    (rename) rename algo-visualization to sorting-visualizer

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/algo-visualization/" : "/",
+    process.env.NODE_ENV === "production" ? "/sorting-visualizer/" : "/",
   transpileDependencies: ["vuetify"],
 };
